572bcb33e940bd825cfda731b6f2c85264cc382e
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-10-06 20:09:18 +0200

    install bootstrap

---
+++ src/js/bootstrapLoader.js
@@ -0,0 +1,8 @@
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Import custom Bootstrap scss
+import './../scss/custom.scss'
+
+// Import global style
+import './../scss/global.scss'
